545f4a43e54bed2162b814b55059438960ddc15e
keynant <52064993+keynant@users.noreply.github.com>
2019-09-06 12:06:18 +0300

    Update program.py

--- program.py
+++ program.py
@@ -1,5 +1,8 @@
 from findfileapi import FindFiles
+from findfileapi import ProcUtil
+
 
 FindFiles.findFile('c:\k', 'K.TXT')
 FindFiles.findExt('c:\k', '.TXT')
 FindFiles.findBiggest('c:\k')
+ProcUtil.showProc()
