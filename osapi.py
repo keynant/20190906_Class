24566bf7b7721f21ff8289f6c9074a44de75bf61
keynant <52064993+keynant@users.noreply.github.com>
2019-09-06 11:29:33 +0300

    Create osapi.py

---
+++ osapi.py
@@ -0,0 +1,36 @@
+import os
+class FindFiles:
+    def findFile(path, filename):
+        for dirpath, dirnames, filenames in os.walk(path):
+            for file1 in filenames:
+                if file1.lower() == filename.lower():
+                    print(f'file {file1} is found in {dirpath}')
+    
+    def findExt(path, ext):
+        for dirpath, dirnames, filenames in os.walk(path):
+            for file1 in filenames:
+                file = os.path.splitext(file1)
+                if file[1].lower() == ext.lower():
+                    print(file1)
+    
+    def findBiggest(path):
+        biggestSize = -1
+        biggestFile = ''
+    
+        for dirpath, dirnames, filenames in os.walk(path):
+            for file1 in filenames:
+                if os.stat(os.path.join(dirpath, file1)).st_size > biggestSize:
+                    biggestSize = os.stat(dirpath + '\\' + file1).st_size
+                    biggestFile = dirpath + '\\' + file1
+        print (f'The biggest file is {biggestFile} at {biggestSize} bytes')
+
+
+def main():
+    findFile('c:\k', 'K.TXT')
+    findExt('c:\k', '.TXT')
+    findBiggest('c:\k')
+    print(os.environ["PATH"])
+
+
+if __name__ == "__main__":
+    main()
